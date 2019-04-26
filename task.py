a5a080a0728c4eabb88b28aebb3fdb1fedc58670
KrzysiekJa <45745634+KrzysiekJa@users.noreply.github.com>
2019-04-26 13:08:36 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,10 @@
 import matrix as mtx
 
-matrix_1 = mtx.matrix(4,5,6,7)
-matrix_2 = mtx.matrix(2,2,2,1)
+matrix_1 = mtx.Matrix(4,5,6,7)
+matrix_2 = mtx.Matrix(2,2,2,1)
 
 matrix_3 = matrix_2.add(matrix_1)
+
+print(matrix_1)
+print(matrix_2)
+print(matrix_3)
