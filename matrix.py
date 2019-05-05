ce963f452f5612a2da384b0012f0669d62aa88e9
KrzysiekJa <45745634+KrzysiekJa@users.noreply.github.com>
2019-05-05 16:24:14 +0200

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,30 +1,72 @@
+import math
+from itertools import starmap
+from operator  import mul
+
 class Matrix:
-    def __init__(self, row1column1 = 0, row1column2 = 0, row2column1 = 0, row2column2 = 0):
-        self.row1column1 = row1column1
-        self.row1column2 = row1column2
-        self.row2column1 = row2column1
-        self.row2column2 = row2column2
-        
-    def add(self,new_one):
-        r1c1 = self.row1column1 + new_one.row1column1
-        r1c2 = self.row1column2 + new_one.row1column2
-        r2c1 = self.row2column1 + new_one.row2column1
-        r2c2 = self.row2column2 + new_one.row2column2
-        
-        return r1c1, r1c2, r2c1, r2c2
+    def __init__(self, list):
+        if math.sqrt(len(list)) != int(math.sqrt(len(list))):
+            raise ValueError("It's not right size of NxN matrix.")
+        else:
+            self.list   = [i for i in list]
+            self.length = int(math.sqrt(len(list)))
     
-    def dotProduct(self,new_one):
-        r1c1 = self.row1column1 * new_one.row1column1
-        r1c2 = self.row1column2 * new_one.row1column2
-        r2c1 = self.row2column1 * new_one.row2column1
-        r2c2 = self.row2column2 * new_one.row2column2
-        
-        return r1c1 + r1c2 + r2c1 + r2c2
+    def __str__(self):
+        string = ""
+        for i, elem in enumerate(self.list):
+           string += str(elem) + " "
+           if (i % self.length == self.length - 1):
+               string += '\n'
+        return string
     
-    def crossProduct(self,new_one):
-        r1c1 = self.row1column1 * new_one.row1column1 + self.row1column2 * new_one.row2column1
-        r1c2 = self.row1column1 * new_one.row1column2 + self.row1column2 * new_one.row2column2
-        r2c1 = self.row2column1 * new_one.row1column1 + self.row2column2 * new_one.row2column1
-        r2c2 = self.row2column1 * new_one.row1column2 + self.row2column2 * new_one.row2column2
+    def __radd__(self, other):
+        return Matrix([i + other for i in self.list])
+    
+    def __add__(self, other): 
+        return Matrix([i + other for i in self.list])
         
-        return r1c1, r1c2, r2c1, r2c2
+    def __iadd__(self, other):
+        if type(other) == int:
+            self.list = [i + other for i in self.list]
+            return self
+        elif type(other) == Matrix:
+            self.list = [elem + other.list[i] for i, elem in enumerate(self.list)]
+            return self
+        else:
+            raise TypeError("Type not excepted.")
+    
+    def __isub__(self, other):
+        if type(other) == int:
+            self.list = [i - other for i in self.list]
+            return self
+        elif type(other) == Matrix:
+            self.list = [elem - other.list[i] for i, elem in enumerate(self.list)]
+            return self
+        else:
+            raise TypeError("Type not excepted.")
+    
+    def __mul__(self, other):
+        return Matrix([elem * other.list[i] for i, elem in enumerate(self.list)])
+    
+    def __matmul__(self, other):
+        our_list   = []
+        tmp        = []
+        other_list = [[] for i in range(other.length)]
+        
+        append = our_list.append
+        for i, elem in enumerate(self.list):
+            tmp.append(elem)
+            if (i % self.length == self.length - 1):
+                append(tmp)
+                tmp = []
+                
+        for i, elem in enumerate(other.list):
+            other_list[i % self.length].append(elem)
+        
+        return Matrix([sum(starmap(mul, zip(row, col))) for row in our_list for col in other_list])
+    
+    def add(self, new_one):
+        if self.length == new_one.length:
+            result = [x + y for x, y in zip(self.list, new_one.list)]
+            return result
+        else:
+            raise ValueError("Matrices are not similar.")
--- task.py
+++ task.py
@@ -1,10 +1,38 @@
 import matrix as mtx
 
-matrix_1 = mtx.Matrix(4,5,6,7)
-matrix_2 = mtx.Matrix(2,2,2,1)
 
-matrix_3 = matrix_2.add(matrix_1)
+def main():
+    matrix_1 = mtx.Matrix([4,5,61,7,2,5,24,-33,1])
+    matrix_2 = mtx.Matrix([2,5,-2,1,4,-13,2,7,9])
+    print(matrix_1)
+    
+    matrix_3 = matrix_1 + 2
+    print("3)\n",matrix_3)
+    
+    matrix_4 = 2 + matrix_3
+    print(matrix_4)
+    
+    print(matrix_2)
+    
+    matrix_5  = mtx.Matrix([0 for i in range(9)])
+    matrix_2 -= 2
+    print(matrix_2)
+    matrix_5 += matrix_2
+    print("5)\n",matrix_5)
+    
+    matrix_6  = mtx.Matrix([1 for i in range(9)])
+    matrix_2 += 3
+    print(matrix_2)
+    matrix_6 -= matrix_2
+    print(matrix_6)
+    
+    matrix_7 = matrix_2 * matrix_3
+    print(matrix_7)
+    
+    matrix_8 = matrix_5 @ matrix_3
+    print(matrix_8)
 
-print(matrix_1)
-print(matrix_2)
-print(matrix_3)
+
+
+if __name__=='__main__':
+    main()
