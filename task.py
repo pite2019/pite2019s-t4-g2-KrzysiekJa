a9523544f7e97da10152558f7f4418ce4c9ae85c
KrzysiekJa <45745634+KrzysiekJa@users.noreply.github.com>
2019-05-05 16:48:28 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -7,7 +7,7 @@ def main():
     print(matrix_1)
     
     matrix_3 = matrix_1 + 2
-    print("3)\n",matrix_3)
+    print(matrix_3)
     
     matrix_4 = 2 + matrix_3
     print(matrix_4)
@@ -18,7 +18,7 @@ def main():
     matrix_2 -= 2
     print(matrix_2)
     matrix_5 += matrix_2
-    print("5)\n",matrix_5)
+    print(matrix_5)
     
     matrix_6  = mtx.Matrix([1 for i in range(9)])
     matrix_2 += 3
@@ -31,7 +31,11 @@ def main():
     
     matrix_8 = matrix_5 @ matrix_3
     print(matrix_8)
-
+    
+    matrix_9 = mtx.Matrix([16,-10,6,7,43,5,0,0,-4,34,1,0,12,1,-5,-2])
+    print(matrix_9)
+    
+    matrix_10 = mtx.Matrix([16,-10,6,7,5,0,0,-4,34,1,0,12,1,-5,-2])
 
 
 if __name__=='__main__':
